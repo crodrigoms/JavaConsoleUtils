b1e0ac4887a7af2667bc3b6a2e4435b40d531ac7
1201367_CarlosSantos <1201367@isep.ipp.pt>
2023-06-24 14:09:25 +0100

    Innitial Commit

---
+++ src/main/java/MenuOption.java
@@ -0,0 +1,18 @@
+public class MenuOption {
+
+    private String header;
+    private Runnable runnable;
+
+    public MenuOption(String header, Runnable runnable) {
+        this.header = header;
+        this.runnable = runnable;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public Runnable getRunnable() {
+        return runnable;
+    }
+}
